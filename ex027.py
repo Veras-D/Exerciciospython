6817d18f1275c31a989df19f0937ba1e06025e35
Danilo Veras <dveras2310@gmail.com>
2023-01-08 18:00:36 -0300

    ex 27

--- ex027.py
+++ ex027.py
@@ -4,3 +4,5 @@ L = N.split()
 print(f'Seu primeiro nome é: {L[0]}.')
 NV = len(L) - 1
 print(f'Seu ultimo nome é: {L[NV]}.')
+# O [-1] pode ser utilizado para se referir ao último objeto de uma lista, assim como [-2] seria a penúltima e assim
+# por diante.
