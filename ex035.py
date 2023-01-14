07c27477415b2fdf5baa4430982ec5a0b1f67531
Danilo Veras <dveras2310@gmail.com>
2023-01-14 03:42:30 -0300

    ex035

---
+++ ex035.py
@@ -0,0 +1,12 @@
+l1 = float(input('Digite o primeiro seguimento do triangulo: '))
+l2 = float(input('Digite o segundo seguimento do triangulo: '))
+l3 = float(input('Digite o terceiro seguimento do triangulo: '))
+if l1 + l2 <= l3:
+    print('Esses seguimentos não formam um triangulo.')
+elif l2 + l3 <= l1:
+    print('Esses seguimentos não formam um triangulo.')
+elif l1 + l3 <= l2:
+    print('Esses seguimentos não formam um triangulo.')
+else:
+    print('Esses seguimentos formam um triangulo.')
+# podia ter deixado todas as condições em um unico if
