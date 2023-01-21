4c80b23836a0abc7eff7c9a76054a45c4fdd8854
Danilo Veras <dveras2310@gmail.com>
2023-01-21 06:13:13 -0300

    ex038

---
+++ ex038.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite outro numero: '))
+if n1 > n2:
+    print(f'{n1:.2f} é maior que {n2:.2f}.')
+elif n2 > n1:
+    print(f'{n2:.2f} é maior que {n1:.2f}.')
+elif n1 == n2:
+    print(f'{n1:.2f}={n2:.2f}.')
+else:
+    print('Opção não valida.')
