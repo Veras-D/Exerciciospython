90cfabee3427956b79e50cea6356d988ae7cddbd
Danilo Veras <dveras2310@gmail.com>
2023-04-03 15:49:41 -0300

    teste derivada

--- derivada.py
+++ derivada.py
@@ -1,6 +1,8 @@
 import sympy
 import math
 x = sympy.symbols('x')
-funcao = x**2 + 2*x - math.e**(-2*x)
+pi = sympy.symbols('pi')
+e = math.e
+funcao = ((pi * x**3) / 6) + e**(2*x)
 derivada = sympy.diff(funcao, x)
 print(derivada)
