0e30ef25ded5e2487a255b620ec9c2dcd535f320
Danilo Veras <dveras2310@gmail.com>
2023-04-05 18:38:01 -0300

    ex055

--- ex055.py
+++ ex055.py
@@ -1,1 +1,8 @@
-print('teste')
+pesos = []
+pesos_2 = []
+for cont in range(0, 5):
+    peso = float(input(f'Qual o peso da {cont + 1}° pessoa? '))
+    pesos_2.append(peso)
+    pesos.append(peso)
+pesos.sort()
+print(f'A {pesos_2.index(pesos[4]) + 1}° é a pessoa mais pesada pesando {pesos[4]} kg e a {pesos_2.index(pesos[0]) + 1} pessoa é a mais leve pesando {pesos[0]} kg.')
