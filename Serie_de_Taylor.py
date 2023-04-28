d6a3bb56f8720c9c4b61346030fdbb3e2abf611e
Danilo Veras <dveras2310@gmail.com>
2023-04-28 14:08:57 -0300

    implementações

--- Serie_de_Taylor.py
+++ Serie_de_Taylor.py
@@ -2,19 +2,22 @@ import sympy
 from math import *
 x = sympy.symbols('x')
 a = sympy.symbols('a')
-fun = f'(sin({a}) + 1)**0.5'
+# (2*((tan({a})-sin({a})))-{a}**3)/{a}**3
+fun = f'(sin({a})+1)**0.5'
 derv = 0
 n = int(input('Insira o n: '))
 maclaurin = []
 for n in range(0, n + 1):
     if n == 0:
         print(fun)
-        maclaurin.append(sympy.sympify(fun) * x ** n / factorial(n))
+        maclaurin.append(sympy.sympify(fun) * (x - a) ** n / factorial(n))
     else:
         derv = sympy.diff(fun)
         print(derv)
-        maclaurin.append(derv * x**n / factorial(n))
+        maclaurin.append(derv * (x - a) ** n / factorial(n))
         fun = derv
 print(maclaurin)
-maclaurin_soma = sum(maclaurin)
-print(maclaurin_soma.subs({a: 0}))
+maclaurin_soma = sum(maclaurin).subs({a: 0})
+print(maclaurin_soma)
+aprox = maclaurin_soma.subs({x: 0})
+print(aprox)
