ea738dc29782d2c85c3f150aaf00f3f3a09a6c5d
Danilo Veras <dveras2310@gmail.com>
2023-06-17 14:44:27 -0300

    Trabalho CNB

--- Método_do_ponto_fixo.py
+++ Método_do_ponto_fixo.py
@@ -6,12 +6,13 @@ a = 1
 b = 1.5
 c = (a + b) / 2
 x = Symbol('x')
+fun_0 = e**x - tan(x)
 fun = atan(e**x)
 erro = 10 ** -3
 n = 0
 x0 = fun.subs(x, c)
 
-while n < 100 and abs(e**x0 - tan(x0)) > erro:
+while n < 100 and abs(fun_0.subs({x: x0})) > erro:
     c = x0
     x0 = fun.subs(x, c)
     print(f'Iteração {n}: {x0}')
