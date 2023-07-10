770e4fcf5052c03cc51e56a67f05b7c729fe1f13
Veras <dveras2310@gmail.com>
2023-07-10 10:32:20 -0300

    Ciclo Brayton - Seleção

---
+++ Scripts_Científicos/Ciclo_Brayton.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+Select = int(input('Escolha Uma Opção: \n[1] - Ciclo simples ideal\n[2] - Ciclo simples com irreversibilidade\n[3] - '
+                   'Ciclo com regenerador ideal\n[4] - Ciclo com regenerador irreversível\n[5] - Ciclo com '
+                   'Inter-Resfriamento ideal\n[7] - Ciclo com Inter-Resfriamento irreversível'))
