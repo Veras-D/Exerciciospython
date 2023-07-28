b058a483fee8732b35619fda9e3dd9aa54619e3c
Veras-D <dveras2310@gmail.com>
2023-07-28 15:59:15 -0300

    Ex057

---
+++ Curso_Em_Video/ex057.py
@@ -0,0 +1,5 @@
+sexo = str(input('Escolha um sexo [M/F]: ')).upper()
+while sexo != 'M' and sexo != 'F':
+    print('Escolha uma opção valida.')
+    sexo = str(input('Escolha um sexo [M/F]: ')).upper()
+print('FIM')
